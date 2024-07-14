128460da45033bae7afcdefa88d90b6ced3280ba
shun2218-dev <kohaku1015game@gmail.com>
2024-07-15 03:27:21 +0900

    feat: 🎸 add register form

---
+++ components/projects/PasswordInput/PasswordInput.tsx
@@ -0,0 +1,44 @@
+import type { FC } from 'react';
+import type { UseFormRegister } from 'react-hook-form';
+import type { LoginInputs, RegisterInputs } from '@/types';
+
+import { Input } from '@nextui-org/input';
+import { useToggle } from 'react-use';
+
+import PasswordToggleButton from '../PasswordToggleButton/PasswordToggleButton';
+
+type Props = {
+  label: string;
+  register: UseFormRegister<RegisterInputs | LoginInputs>;
+  errorMessage: string | undefined;
+  isConfirm?: boolean;
+};
+
+const PasswordInput: FC<Props> = ({
+  label,
+  register,
+  errorMessage,
+  isConfirm = false,
+}) => {
+  const [isVisible, toggleVisibility] = useToggle(false);
+
+  return (
+    <Input
+      isRequired
+      autoComplete="password"
+      endContent={
+        <PasswordToggleButton
+          isVisible={isVisible}
+          toggleVisibility={toggleVisibility}
+        />
+      }
+      errorMessage={errorMessage}
+      isInvalid={!!errorMessage}
+      label={label}
+      type={isVisible ? 'text' : 'password'}
+      {...register(isConfirm ? 'confirmPassword' : 'password')}
+    />
+  );
+};
+
+export default PasswordInput;
--- types/index.ts
+++ types/index.ts
@@ -3,3 +3,15 @@ import { SVGProps } from 'react';
 export type IconSvgProps = SVGProps<SVGSVGElement> & {
   size?: number;
 };
+
+export type RegisterInputs = {
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+export type LoginInputs = {
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
