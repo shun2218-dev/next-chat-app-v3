fa8a565297c718aff17c64d7902a7c4504bed0c0
shun2218-dev <kohaku1015game@gmail.com>
2024-08-03 15:05:42 +0900

    refactor: 💡 add redirect by auth status
    remove AuthGuard component and replace it with auth check by middleware

--- app/mypage/layout.tsx
+++ app/mypage/layout.tsx
@@ -6,12 +6,10 @@ export default function MyPageLayout({
   children: React.ReactNode;
 }) {
   return (
-    // <AuthGuard>
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10 w-[100%]">
       <div className="inline-block max-w-lg text-center justify-center w-[100%]">
         {children}
       </div>
     </section>
-    // </AuthGuard>
   );
 }
