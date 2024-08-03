4a83684da717bdfe273b347d72f8987251163a6c
shun2218-dev <kohaku1015game@gmail.com>
2024-08-03 15:08:54 +0900

    feat: 🎸 add chatroom menu and icon at start content each menu

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -1,12 +1,17 @@
 'use client';
 
 import { Card, CardBody, CardHeader } from '@nextui-org/card';
-import { Listbox, ListboxItem } from '@nextui-org/listbox';
+import { Listbox, ListboxItem, ListboxSection } from '@nextui-org/listbox';
 import { Link } from '@nextui-org/link';
 import { User } from '@nextui-org/user';
 import { useSession } from 'next-auth/react';
+import QuestionAnswerOutlinedIcon from '@mui/icons-material/QuestionAnswerOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import VpnKeyOutlinedIcon from '@mui/icons-material/VpnKeyOutlined';
 
 import { title } from '@/components/primitives';
+import { IconWrapper } from '@/components/uiParts/IconWrapper/IconWrapper';
 
 export default function MyPage() {
   const { data: session } = useSession();
@@ -22,22 +27,63 @@ export default function MyPage() {
       </CardHeader>
       <CardBody className="w-[90%] mx-auto">
         <User name={session?.user.name} />
-        <Listbox aria-label="menu" className="mt-5">
-          <ListboxItem key="profile">
-            <Link className="text-inherit" href="/mypage/profile">
-              Profile
-            </Link>
-          </ListboxItem>
-          <ListboxItem key="email">
-            <Link className="text-inherit" href="/mypage/email">
-              Email
-            </Link>
-          </ListboxItem>
-          <ListboxItem key="password">
-            <Link className="text-inherit" href="/mypage/password">
-              Password
-            </Link>
-          </ListboxItem>
+        <Listbox aria-label="mypage-menu" className="mt-5">
+          <ListboxSection title="Content">
+            <ListboxItem
+              key="chatroom"
+              startContent={
+                <IconWrapper className="bg-slate/10 text-slate">
+                  <QuestionAnswerOutlinedIcon />
+                </IconWrapper>
+              }
+              textValue="Chatroom"
+            >
+              <Link className="text-inherit" href="/chatroom">
+                Chatroom
+              </Link>
+            </ListboxItem>
+          </ListboxSection>
+          <ListboxSection title="User">
+            <ListboxItem
+              key="profile"
+              startContent={
+                <IconWrapper className="bg-primary/10 text-primary">
+                  <AccountCircleOutlinedIcon />
+                </IconWrapper>
+              }
+              textValue="Profile"
+            >
+              <Link className="text-inherit" href="/mypage/profile">
+                Profile
+              </Link>
+            </ListboxItem>
+            <ListboxItem
+              key="email"
+              startContent={
+                <IconWrapper className="bg-teal-400/10 text-teal-400">
+                  <EmailOutlinedIcon />
+                </IconWrapper>
+              }
+              textValue="Email"
+            >
+              <Link className="text-inherit" href="/mypage/email">
+                Email
+              </Link>
+            </ListboxItem>
+            <ListboxItem
+              key="password"
+              startContent={
+                <IconWrapper className="bg-warning/10 text-warning">
+                  <VpnKeyOutlinedIcon />
+                </IconWrapper>
+              }
+              textValue="Password"
+            >
+              <Link className="text-inherit" href="/mypage/password">
+                Password
+              </Link>
+            </ListboxItem>
+          </ListboxSection>
         </Listbox>
       </CardBody>
     </Card>
