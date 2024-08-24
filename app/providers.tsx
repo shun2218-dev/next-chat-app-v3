eaa9fe8b13d0665583e0679910c5ec226931b41b
shun2218-dev <kohaku1015game@gmail.com>
2024-08-25 00:56:51 +0900

    refactor: 💡 remove Redux

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,19 +1,12 @@
 'use client';
 
 import type { ReactNode } from 'react';
-import type { AppStore } from '@/libs/store';
 
-import { useRef } from 'react';
-import { Provider } from 'react-redux';
 import { NextUIProvider } from '@nextui-org/system';
 import { useRouter } from 'next/navigation';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { ThemeProviderProps } from 'next-themes/dist/types';
-import { PersistGate } from 'redux-persist/integration/react';
-import { Persistor, persistStore } from 'redux-persist';
 import { SessionProvider } from 'next-auth/react';
-
-import { makeStore, persistor } from '@/libs/store';
 
 export interface ProvidersProps {
   children: ReactNode;
@@ -22,26 +15,12 @@ export interface ProvidersProps {
 
 export function Providers({ children, themeProps }: ProvidersProps) {
   const router = useRouter();
-  const storeRef = useRef<AppStore>();
-  const persistorRef = useRef<Persistor>(persistor);
-
-  if (!storeRef.current) {
-    // Create the store instance the first time this renders
-    const store = makeStore();
-
-    storeRef.current = store;
-    persistorRef.current = persistStore(store);
-  }
 
   return (
     <SessionProvider>
-      <Provider store={storeRef.current}>
-        <PersistGate loading={null} persistor={persistorRef.current}>
-          <NextUIProvider navigate={router.push}>
-            <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
-          </NextUIProvider>
-        </PersistGate>
-      </Provider>
+      <NextUIProvider navigate={router.push}>
+        <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+      </NextUIProvider>
     </SessionProvider>
   );
 }
