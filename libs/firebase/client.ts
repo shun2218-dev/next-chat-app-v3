1a85ea6e2d32a0002b91ea4b1dc48469a07291c5
shun2218-dev <kohaku1015game@gmail.com>
2024-10-06 09:45:41 +0900

    fix: 🐛 firebase environment variable error corrected

--- libs/firebase/client.ts
+++ libs/firebase/client.ts
@@ -13,7 +13,8 @@ import { getStorage } from 'firebase/storage';
 const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_DATABASE_URL,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_APP_ID,
