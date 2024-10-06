b47f14b94ced99f838c5451bf78b94d8953a8823
shun2218-dev <kohaku1015game@gmail.com>
2024-10-06 09:53:18 +0900

    feat: 🎸 create sidebar display friend user list

---
+++ components/projects/AddFriend/AddFriend.tsx
@@ -0,0 +1,87 @@
+import { Fragment, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button } from '@nextui-org/button';
+import { Input } from '@nextui-org/input';
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+} from '@nextui-org/modal';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { REQUIRED_ONLY } from '@/schema/formSchema';
+import { useFriend } from '@/hooks/useFriend';
+
+export const AddFriend = () => {
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+    reset,
+  } = useForm<{ friendId: string }>({
+    resolver: zodResolver(z.object({ friendId: REQUIRED_ONLY })),
+  });
+  const { isOpen, onOpen, onClose, onOpenChange } = useDisclosure();
+  const { addFriend, getFriendUsers } = useFriend();
+
+  const onSubmit: SubmitHandler<{ friendId: string }> = async ({
+    friendId,
+  }) => {
+    try {
+      const result = await addFriend(friendId);
+
+      if (result.roomId) {
+        await getFriendUsers();
+        router.push(`/chatroom/${result.roomId}`);
+      }
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        console.error(err.message);
+      }
+    } finally {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    reset();
+  }, [isOpen]);
+
+  return (
+    <Fragment>
+      <Button onPress={onOpen}>Add Friend</Button>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <ModalHeader className="flex flex-col gap-1">
+                Enter the friend ID
+              </ModalHeader>
+              <ModalBody>
+                <Input
+                  {...register('friendId')}
+                  errorMessage={errors.friendId?.message}
+                  isInvalid={!!errors.friendId?.message}
+                />
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" type="submit">
+                  Add
+                </Button>
+              </ModalFooter>
+            </form>
+          )}
+        </ModalContent>
+      </Modal>
+    </Fragment>
+  );
+};
---
+++ components/projects/FriendUserList/FriendUserList.tsx
@@ -0,0 +1,87 @@
+'use client';
+import { Fragment, useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { Listbox, ListboxItem } from '@nextui-org/listbox';
+import { Avatar } from '@nextui-org/avatar';
+import { Spinner } from '@nextui-org/spinner';
+
+import { ErrorPanel } from '@/components/uiParts/ErrorPanel/ErrorPanel';
+import { useFriend } from '@/hooks/useFriend';
+import { AddFriend } from '@/components/projects/AddFriend/AddFriend';
+
+export const FriendUserList = () => {
+  const [selectedKeys, setSelectedKeys] = useState<
+    'all' | Set<string | number>
+  >(new Set<string>([]));
+  const router = useRouter();
+  const { getFriendUsers, friendUsers } = useFriend();
+
+  useEffect(() => {
+    (async () => {
+      if (friendUsers === null) {
+        await getFriendUsers();
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (Array.from(selectedKeys).length > 0) {
+      router.push(`/chatroom/${Array.from(selectedKeys)[0]}`);
+    } else {
+      router.push(`/chatroom`);
+    }
+  }, [selectedKeys]);
+
+  if (friendUsers === null) {
+    return (
+      <div className="w-full h-1/2 flex justify-center items-center">
+        <Spinner label="loading..." size="md" />
+      </div>
+    );
+  }
+
+  return (
+    <Fragment>
+      <Listbox
+        bottomContent={<AddFriend />}
+        classNames={{
+          base: 'max-w-xs',
+          list: 'max-h-[300px]',
+        }}
+        defaultSelectedKeys={['1']}
+        emptyContent={
+          <ErrorPanel errorMsg="There may be no users other than you. Invite others to join this chat!" />
+        }
+        items={friendUsers}
+        label="Assigned to"
+        selectedKeys={selectedKeys}
+        selectionMode="single"
+        variant="flat"
+        onSelectionChange={setSelectedKeys}
+      >
+        {(item) => (
+          <ListboxItem
+            key={item.chatId}
+            className="UserList"
+            textValue={item.name ?? 'Unkonown'}
+          >
+            <div className="flex gap-2 items-center">
+              <Avatar
+                showFallback
+                alt={item.name ?? 'Unkonown'}
+                className="flex-shrink-0"
+                name={item.name ?? 'Unkonown'}
+                size="sm"
+                src={item.image!}
+              />
+              <div className="md:flex md:flex-col hidden">
+                <span className="text-small">{item.name}</span>
+                <span className="text-tiny text-default-400">{item.email}</span>
+              </div>
+            </div>
+          </ListboxItem>
+        )}
+      </Listbox>
+    </Fragment>
+  );
+};
