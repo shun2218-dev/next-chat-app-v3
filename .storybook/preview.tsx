8d307bd4566cfb0e10f8c36eaa8cde42cc8ba414
shun2218-dev <kohaku1015game@gmail.com>
2025-04-27 23:45:12 +0900

    feat: 🎸 add msw for mock api

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -4,16 +4,21 @@ import React from 'react';
 import type { Preview } from '@storybook/react';
 import { NextUIProvider } from '@nextui-org/react';
 import { SessionProvider } from 'next-auth/react';
+import { initialize, mswLoader } from 'msw-storybook-addon';
+import { http, HttpResponse } from 'msw';
 
 const mockSession = {
   user: {
-    id: 'use1',
+    id: 'user1',
     name: 'John Doe',
-    email: 'mock@example.com',
     image: 'https://placehold.jp/150x150.png',
+    email: 'storybook@example.com',
+    role: 'USER',
   },
-  expires: new Date(Date.now() + 1000 * 60 * 60 * 24).toString(), // 1 day
+  expires: new Date(Date.now() + 1000 * 60 * 5).toISOString(), // 5 minutes
 };
+
+initialize();
 
 const preview: Preview = {
   parameters: {
@@ -26,6 +31,16 @@ const preview: Preview = {
     nextjs: {
       appDirectory: true,
     },
+    msw: {
+      handlers: [
+        http.get('/api/auth/session', () => {
+          return HttpResponse.json({
+            ...mockSession,
+            expires: new Date(Date.now() + 1000 * 60 * 5).toISOString(),
+          });
+        }),
+      ],
+    },
   },
   decorators: [
     (Story) => (
@@ -36,6 +51,7 @@ const preview: Preview = {
       </SessionProvider>
     ),
   ],
+  loaders: [mswLoader],
 };
 
 export default preview;
