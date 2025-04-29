36250efc6c20cb71dd95c495037b59080e4fab13
shun2218-dev <kohaku1015game@gmail.com>
2025-04-30 00:42:49 +0900

    test: 💍 add unit test for ErrorPanel

--- components/uiParts/ErrorPanel/ErrorPanel.tsx
+++ components/uiParts/ErrorPanel/ErrorPanel.tsx
@@ -6,7 +6,10 @@ type Props = {
 
 export const ErrorPanel: FC<Props> = ({ children }) => {
   return (
-    <p className="ErrorPanel text-red-500 bg-red-500 bg-opacity-10 text-center p-3 mb-3 rounded-md">
+    <p
+      className="ErrorPanel text-red-500 bg-red-500 bg-opacity-10 text-center p-3 mb-3 rounded-md"
+      data-testid="error-panel"
+    >
       {children}
     </p>
   );
